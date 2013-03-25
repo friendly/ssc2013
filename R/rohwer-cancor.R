7e819f7eecf76e2a47f059beaa0af93fa83b2483
Michael Friendly <friendly@yorku.ca>
2013-03-25 12:13:24 -0400

    added section on CCA, plus other changes

--- R/rohwer-cancor.R
+++ R/rohwer-cancor.R
@@ -6,11 +6,20 @@ Y <- as.matrix(Rohwer[,3:5])   # the aptitude/ability variables
 
 cc <- cancor(X, Y, set.names=c("PA", "Ability"))
 
+cc <- cancor(cbind(SAT, PPVT, Raven) ~ n + s + ns + na + ss, data=Rohwer,
+          set.names=c("PA", "Ability"))
+
 cd("c:/documents/presentations/ssc2013/figures")
 pdf(file="rohwer-cancor.pdf", height=6, width=6)
 op <- par(mar=c(4,4,1,1)+.1)
 plot(cc, smooth=TRUE, id.n=3, ellipse.args=list(fill=TRUE), col.smooth="blue", lwd=3,
 	cex.lab=1.4, id.cex=1.25, pch=20, cex=1.5)
 text(-2,1.9, "Canonical correlation analysis: Abilities ~ PA tasks", pos=4, cex=1.3)
+par(op)
+dev.off()
+
+pdf(file="rohwer-cancor-HE.pdf", height=5.4, width=6)
+op <- par(mar=c(4,4,1,1)+.1)
+heplot(cc, scale=1.4, var.cex=1.5, var.lwd=3, prefix="Y canonical dimension", cex.lab=1.25, lwd=2)
 par(op)
 dev.off()
